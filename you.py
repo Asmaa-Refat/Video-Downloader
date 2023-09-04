940f73cb1bd0e04c9f23d3211c9c077c3da360bc
Asmaa Refat <81772505+Asmaa-Refat@users.noreply.github.com>
2023-09-04 12:38:18 +0300

    Update you.py

--- you.py
+++ you.py
@@ -7,16 +7,16 @@ url = input("Enter the URL: ")
 
 allStreams = YouTube(url).streams.all() #all the qualities (streams)
 
+#printing the different qualities
 i = 0
 for stream in allStreams:
     print(str(i+1) + " : " + str(stream))
     i+=1
 
-
 streamChoiceIndex = int(input("Choose the stream quality index you want:  "))
-print("please choose the direcroty to download in! ")
-folderName = filedialog.askdirectory()
+print("please choose the directory to download in! ")
 
-allStreams[streamChoiceIndex-1].download(folderName)
+path = filedialog.askdirectory()
+allStreams[streamChoiceIndex-1].download(path)
 
 print("The Video has been downloaded, Yaaaay!")
